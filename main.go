cec7bdda70d04de7765363f2fc4f3e0516f84b34
Nguyễn Minh Tuấn <nguyenminhtuan06012004@gmail.com>
2025-10-25 09:19:24 +0700

    done

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/tenuser/myapp/handlers"
+	"github.com/tenuser/myapp/middlewares"
 	"github.com/tenuser/myapp/repositories"
 	"github.com/tenuser/myapp/services"
 )
@@ -15,12 +16,17 @@ func main() {
 	handler := handlers.NewTaskHandler(service)
 
 	router := gin.Default()
+
+	// Global logging middleware (prints: [METHOD] URL - STATUS_CODE)
+	router.Use(middlewares.LoggingMiddleware())
+
 	api := router.Group("/api")
 	api.GET("/tasks", handler.ListTasks)
-	api.POST("/tasks", handler.CreateTask)
+	// protect creating, updating and deleting with a simple API key
+	api.POST("/tasks", middlewares.SimpleAuthMiddleware(), handler.CreateTask)
 	api.GET("/tasks/:id", handler.GetTask)
-	api.PUT("/tasks/:id", handler.UpdateTask)
-	api.DELETE("/tasks/:id", handler.DeleteTask)
+	api.PUT("/tasks/:id", middlewares.SimpleAuthMiddleware(), handler.UpdateTask)
+	api.DELETE("/tasks/:id", middlewares.SimpleAuthMiddleware(), handler.DeleteTask)
 
 	if err := router.Run(); err != nil {
 		log.Fatalf("failed to run server: %v", err)
